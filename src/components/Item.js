c3811837451b905603253d819a7913634cf2e077
ASayidov <abdullajonsayidov@gmail.com>
2023-04-05 18:41:04 +0500

    added images

--- src/components/Item.js
+++ src/components/Item.js
@@ -4,11 +4,11 @@ export class Item extends Component {
   render() {
     return (
       <div className='item'>
-        <img src={''} alt="" />
+        <img src={'./img/' + this.props.item.img} alt='test' />
         <h2>{this.props.item.title}</h2>
         <p>{this.props.item.desc}</p>
-        <b>{this.props.item.price}</b>
-        <div className="add-to-cart"></div>
+        <b>{this.props.item.price}$</b>
+        <div className="add-to-cart">+</div>
       </div>
     )
   }
