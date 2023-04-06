e1a760077ece8bb9d779cf9b00378ebce80f84e1
ASayidov <abdullajonsayidov@gmail.com>
2023-04-06 17:04:20 +0500

    added show product in basket

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,26 @@
 import React, { useState } from 'react'
 import { FaShoppingCart } from "react-icons/fa";
+import Order from './Order';
 
-function Header() {
+const showOrders = (props) => {
+  return (
+    <div>
+      {props.orders.map(el => (
+        <Order key={el.id} item={el} />
+      ))}
+    </div>
+  )
+}
+
+const showNothing = () => {
+  return (
+    <div className='empty'>
+      <h2>Товаров нет</h2>
+    </div>
+  )
+}
+
+function Header(props) {
   const [cartOpen, setCartOpen] = useState(false);
   return (
     <header>
@@ -16,7 +35,7 @@ function Header() {
 
         {cartOpen &&
           <div className="shop-cart">
-
+            {[props.orders.length > 0 ? showOrders(props) : showNothing()]}
           </div>
         }
       </div>
